6d64659335fed651b02afcbbccf4f6c9a602667e
Juan Antonio Sosa <juanantonio@typeform.com>
2018-02-14 12:37:15 +0100

    Repository added

---
+++ Interfaces/IToDoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoAppRestAPI.Models;
+
+namespace TodoAppRestAPI.Interfaces
+{
+    public interface IToDoRepository
+    {
+        bool DoesItemExist(string id);
+        IEnumerable<TodoItem> All { get; }
+        TodoItem Find(string id);
+        void Insert(TodoItem item);
+        void Update(TodoItem item);
+        void Delete(string id);
+    }
+}
